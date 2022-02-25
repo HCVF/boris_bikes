30f490c4b5e19fd91d57c38612985ae1733bcb18
Henry <hcvferrari@gmail.com>
2022-02-25 10:57:31 +0000

    boris bikes exercise up to ex. 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,26 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_accessor :bike_rack
+  def initialize
+    @bike_rack = []
+  end
+
+  def bike_rack
+    @bike_rack
+  end
+  
   def release_bike
-    Bike.new
+    return @bike_rack.pop if !@bike_rack.empty?
+    return raise IndexError.new("no bikes")
+  end
+
+  def capacity
+    capacity = 20
+  end
+
+  def dock(bike)
+    return @bike_rack.push(bike) if @bike_rack.length < capacity
+    return raise IndexError.new("rack full")
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,38 @@
 require "docking_station"
 
-describe DockingStation do 
+describe DockingStation do
+
     it { is_expected.to respond_to :release_bike }
+
     it "returns a bike instance" do
-      expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+      bike = Bike.new
+      subject.bike_rack = [bike]
+      expect(subject.release_bike).to be_an_instance_of(Bike)
     end
-    it "test if output of release_bike method responds to working? method" do
-      bike_instance = DockingStation.new.release_bike
-      expect(bike_instance).to respond_to(:working?)
+
+    it "output of release_bike method responds to working? method" do
+      bike = Bike.new
+      expect(bike.working?).to eq(true)
+    end
+    
+    it "check if docking_station responds to dock" do
+      expect(subject).to respond_to(:dock)
+    end
+
+    it "return_bike increases size of bike_rack array" do
+      bike = Bike.new
+      number_bikes = subject.bike_rack.length
+      expect(subject.dock(bike).length).to eq(number_bikes + 1)
+    end
+    
+    it "if user tries to take bike from empty rack they will get an error" do
+      puts subject.bike_rack
+      expect { subject.release_bike }.to raise_error(IndexError)
+    end
+
+    it "fails if user tries to dock bike in full rack" do
+      bike = Bike.new
+      subject.capacity.times { subject.dock(bike) }
+      expect{ subject.dock(bike) }.to raise_error(IndexError)
     end
 end
